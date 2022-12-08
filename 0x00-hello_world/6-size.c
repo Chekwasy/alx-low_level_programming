fafec92b060fd42e568d2f604781e1abfcddc59e
Chekwasy <richardchekwas@gmail.com>
2022-12-08 10:55:23 -0800

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of char: %lu bytes\n", sizeof(char));
+printf("size of int: %lu bytes\n", sizeof(int));
+printf("size of long int: %lu bytes\n", sizeof(long int));
+printf("size of long long int: %lu bytes\n", sizeof(long long int));
+printf("size of float: %lu bytes\n", sizeof(float));
+return (0);
+}
