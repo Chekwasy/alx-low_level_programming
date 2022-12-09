b3725e07238677ce94dc764b9df98f9699b5da83
Chekwasy <richardchekwas@gmail.com>
2022-12-09 11:47:36 -0800

    small letters with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+for (c; c <= 'z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
