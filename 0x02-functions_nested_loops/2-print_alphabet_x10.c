dffc158806e4417ff0d219dd6b7f350e65ee3dbc
Chekwasy <richardchekwas@gmail.com>
2022-12-13 10:58:28 -0800

    print alphabet times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Check description
+ * Description: It prints the alphabet in lowercase fallowe ten times\
+ * d by a new line
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		int a;
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
