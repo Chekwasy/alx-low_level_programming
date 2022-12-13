0c6b5de2557b631a8bd80154f76e2611e535e47a
Chekwasy <richardchekwas@gmail.com>
2022-12-13 06:24:32 -0800

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-	char a = '_';
-	putchar(a);
-	a = 'p';
-	putchar(a);
-	a = 'u';
-	putchar(a);
-	a = 't';
-	putchar(a);
-	a = 'c';
-	putchar(a);
-	a = 'h';
-	putchar(a);
-	a = 'a';
-	putchar(a);
-	a = 'r';
-	putchar(a);
-	putchar('\n');
-	return (0);
+char a = '_';
+putchar(a);
+a = 'p';
+putchar(a);
+a = 'u';
+putchar(a);
+a = 't';
+putchar(a);
+a = 'c';
+putchar(a);
+a = 'h';
+putchar(a);
+a = 'a';
+putchar(a);
+a = 'r';
+putchar(a);
+putchar('\n');
+return (0);
 }
