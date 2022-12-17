90559e1b205ea971d56a2af8609be84c15d0e233
Chekwasy <richardchekwas@gmail.com>
2022-12-17 01:42:14 -0800

    aad

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-	int i, num;
+	int i;
 	long int sum = 0;
 
 	for (i = 1; i < 1024; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum = sum + i;
 		}
 	}
 	printf("%ld\n", sum);
+	return (0);
 }
