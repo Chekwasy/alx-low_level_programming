5d761619ad320dd3da094dc2e576e62bf8a44457
Chekwasy <richardchekwas@gmail.com>
2022-12-17 02:32:28 -0800

    adjust 3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,26 @@
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	int i;
+	unsigned long int a, next;
 
- 
+	a = 1;
+	next = 2;
+
+	printf("%ld, ", a);
+	printf("%ld, ", next);
+
+	for (i = 1; i <= 48; i++)
+	{
+		next = next + a;
+		a = next - a;
+		printf("%ld", next);
+		if (i < 48)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+
+}
