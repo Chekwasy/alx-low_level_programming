e75325aa964d089c4f27dc298e788e8b920ce83c
Chekwasy <richardchekwas@gmail.com>
2022-12-19 05:50:05 -0800

    aaag

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - Check description
+ * Description: It tells if lower case
+ * @a: - An input parameter
+ * @b: - second parameter
+ * d by a new line
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
