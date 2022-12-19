3b166df91946ad78ebe8c6995a24d453f1c94ec6
Chekwasy <richardchekwas@gmail.com>
2022-12-19 12:47:09 -0800

    z

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - Check description
+ * Description: It tells if lower case
+ * @str: - An input parameter
+ * d by a new line
+ * Return: 1 when true and 0 when false
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+		i++;
+	}
+}
