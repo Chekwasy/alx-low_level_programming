6a98f9360a309c084031ccf2cb203006762121fb
Chekwasy <richardchekwas@gmail.com>
2022-12-20 06:26:03 -0800

    cc

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,21 @@
  */
 
 void rev_string(char *s)
+{
+	int i;
+	int a;
+	int b;
+	char arr;
 
-	int len, i, half;
-	char temp;
-
-	for (len = 0; s[len] != '\0'; len++)
-		;
-	i = 0;
-	half = len / 2;
-
-	while (half--)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		temp = s[len - i - 1];
-		s[len - i - 1] = s[i];
-		s[i] = temp;
-		i++;
+	}
+	a = i / 2;
+
+	for (b = 0; b < a; b++)
+	{
+		arr = *(s + b);
+		*(s + b) = *(s + (i - 1 - b));
+		*(s + (i - 1 - b)) = arr;
 	}
 }
