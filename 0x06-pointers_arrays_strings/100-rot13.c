7c1a7a3cbcba685074d1afe8f75958ab5fdfc7c5
Chekwasy <richardchekwas@gmail.com>
2022-12-26 12:10:55 -0800

    aat

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,39 +8,23 @@
  */
 char *rot13(char *str)
 {
-       int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			     'G', 'H', 'I', 'J', 'K', 'L',
-			     'M', 'N', 'O', 'P', 'Q', 'R',
-			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-			     'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			     'T', 'U', 'V', 'W', 'X', 'Y',
-			     'Z', 'A', 'B', 'C', 'D', 'E',
-			     'F', 'G', 'H', 'I', 'J', 'K',
-			     'L', 'M', 'n', 'o', 'p', 'q',
-			     'r', 's', 't', 'u', 'v', 'w',
-			     'x', 'y', 'z', 'a', 'b', 'c',
-			     'd', 'e', 'f', 'g', 'h', 'i',
-			     'j', 'k', 'l', 'm'};
+	int a = 0;
+	int b;
 
-	while (str[indx1])
+	char arr1[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(str + a) != '\0')
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		for (b = 0; b < 52; b++)
 		{
-			if (str[indx1] == alphabet[indx2])
+			if (*(str + a) == arr1[b])
 			{
-				str[indx1] = rot13key[indx2];
+				*(str + a) = arr2[b];
 				break;
 			}
 		}
-
-		indx1++;
-
+		a++;
 	}
 	return (str);
 }
