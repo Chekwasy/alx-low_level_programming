6234fd7bedcd2591efecaf55585c681bf3d5d4c8
Chekwasy <richardchekwas@gmail.com>
2023-01-05 02:02:16 -0800

    q2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,14 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	int n = 0;
 
-	if (*s)
+	if (*s != '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		n = _strlen_recursion(s + 1) + 1;
+		return (n);
 	}
+	else
+		return (0);
 
-	return (len);
 }
