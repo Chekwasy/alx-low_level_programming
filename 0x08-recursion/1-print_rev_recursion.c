b3ec86ba43f44cf3157fbd051c02542e121b47f8
Chekwasy <richardchekwas@gmail.com>
2023-01-04 19:53:57 -0800

    q1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Check description
+ * Description: reverse recursion
+ * @s: - An input parameter
+ * d by a new line
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		s = s + 13;
+	}
+	if (*s != '\n')
+	{
+		putchar(*(s));
+		s--;
+		if (*s != '\n')
+		{
+			_print_rev_recursion(s);
+		}
+		putchar('\n');
+	}
+}
