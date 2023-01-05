2333082b8d77696f837ae0fb32faf8b2294d9196
Chekwasy <richardchekwas@gmail.com>
2023-01-05 00:46:20 -0800

    q1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,16 +3,9 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)
+if (*s != '\0')
 {
-	if (*s != '\0')
-	{
-		s++;
-		_print_rev_recursion(s);
-	}
-	if (*s == '\0')
-	{
-		return;
-	}
-	_putchar(*s);
+	s++;
+	_print_rev_recursion(s);
+	putchar(*s);
 }
