775dc6b9f020a8db6879421adeffa2e44de7b033
Chekwasy <richardchekwas@gmail.com>
2023-01-05 21:54:25 -0800

    q5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
+int findroot(int n, int root);
 
 /**
- * find_sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
+ * findroot - Finds the natural square root of an inputted number.
+ * @n: The number to find the square root of.
  * @root: The root to be tested.
  *
  * Return: If the number has a natural square root - the square root.
  *         If the number does not have a natural square root - -1.
  */
-int find_sqrt(int num, int root)
+int findroot(int n, int root)
 {
-	if ((root * root) == num)
-		return (root);
-
-	if (root == num / 2)
+	if (root == n)
 		return (-1);
-
-	return (find_sqrt(num, root + 1));
+	if (root * root == n)
+		return (root);
+	findroot(n, root + 1);
 }
 
 /**
@@ -29,15 +26,17 @@ int find_sqrt(int num, int root)
  * Return: If n has a natural square root - the natural square root of n.
  *         If n does not have a natural square root - -1.
  */
+
 int _sqrt_recursion(int n)
 {
 	int root = 0;
 
 	if (n < 0)
 		return (-1);
-
-	if (n == 1)
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
 		return (1);
-
-	return (find_sqrt(n, root));
+	else
+		return (findroot(n, root));
 }
