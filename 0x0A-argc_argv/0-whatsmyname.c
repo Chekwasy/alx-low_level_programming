6e07f6bfee25f1d07862dc18664f2f245762efd7
Chekwasy <richardchekwas@gmail.com>
2023-01-06 15:43:02 -0800

    args q0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "main.h"
+/**
+ * main - Check description
+ * Description: It tells if lower case
+ * @argc: - An input parameter
+ * @argv: - Another parameterd
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (1);
+}
