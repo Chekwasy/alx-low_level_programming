40f26ac028ff21c2bc3d5c153fdae0baa02798dd
Chekwasy <richardchekwas@gmail.com>
2023-01-06 23:59:53 -0800

    q4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,29 +10,33 @@
  */
 
 int main(int argc, char *argv[])
-{                                                                 int sum = 0;
-	int i;
+{
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
-	{
+	if (argc < 2)
 		printf("0\n");
-	}
 	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
+			ptr = argv[i];
+			length = strlen(ptr);
 
-			if (*argv[i] >= '0' && *argv[i] <= '9')
+			for (j = 0; j < length; j++)
 			{
-				sum = sum + (atoi(argv[i]));
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+
+			sum += atoi(argv[i]);
 		}
+
 		printf("%d\n", sum);
 	}
-		return (0);
+	return (0);
 }
