b3fcb730e311fae1a67ba934f4fbd8d8a394d5c4
Chekwasy <richardchekwas@gmail.com>
2023-01-08 05:14:27 -0800

    q100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -53,6 +53,11 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
+	if (atoi(argv[1]) == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 	num = atoi(argv[1]);
 	count = 0;
 	for (a = 0; a < 5; a++)
