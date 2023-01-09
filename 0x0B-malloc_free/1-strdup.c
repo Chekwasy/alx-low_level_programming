4141114b5d3c9771e7a96d5ce7c5720b8492b0b9
Chekwasy <richardchekwas@gmail.com>
2023-01-09 07:55:24 -0800

    q1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - Check description
+ * Description: It tells if lower case
+ * @str: - An input parameter
+ * d by a new line
+ * Return: 1 when true and 0 when false
+ */
+char *_strdup(char *str)
+{
+	int len;
+	int i;
+	char *s;
+
+	len = strlen(str);
+	s = malloc(sizeof(char) * len);
+	if (s == NULL || len == 0)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		s[i] = *(str + i);
+	}
+	return (s);
+}
