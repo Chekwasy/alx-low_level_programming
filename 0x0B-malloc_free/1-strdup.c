3e7777a7c56ecf94853aa93889db012e738e405d
Chekwasy <richardchekwas@gmail.com>
2023-01-09 10:41:47 -0800

    q1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,15 +17,15 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	len = strlen(str);
+	len = strlen(str) + 1;
 	s = malloc(sizeof(char) * len);
 	if (s == NULL || str == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < len; i++)
 	{
-		if (i != len)
+		if (i != (len - 1))
 			s[i] = *(str + i);
-		if (i == len)
+		if (i == (len - 1))
 			s[i] = '\0';
 	}
 	return (s);
