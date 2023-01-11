6251d715e259aae1dc297d63549dab8f4787e972
Chekwasy <richardchekwas@gmail.com>
2023-01-11 14:38:17 -0800

    q100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
 /**
- * argstostr - Check description
- * Description: It tells if lower case
- * @ac: - An input parameter
- * @av: -d by a new line
- * Return: 1 when true and 0 when false
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
  */
 char *argstostr(int ac, char **av)
 {
-	char *ptr;
-	int i;
-	int n;
-	int strln;
+	char *str;
+	int arg, byte, index, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+
+	for (arg = 0; arg < ac; arg++)
 	{
-		strln = strlen(av[i]);
-		ptr = malloc(sizeof(char) * strln);
-		if (ptr == NULL)
-			return (NULL);
-		for (n = 0; n <= strln; n++)
-		{
-			if (n != strln)
-			{
-				ptr[n] =*(av[i] + n);
-			}
-			if (n == strln)
-				ptr[n] = '\0';
-		}
-		for (n = 0; n <= strln; n++)
-		{
-			printf("%c", ptr[n]);
-		}
-		printf("\n");
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
 	}
-	return (ptr);
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
 }
