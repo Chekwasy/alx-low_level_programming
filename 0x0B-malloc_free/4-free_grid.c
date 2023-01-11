6024b5527ffef989728c6eccbdc8d38fd1c37378
Chekwasy <richardchekwas@gmail.com>
2023-01-10 19:43:50 -0800

    q4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - Check description
+ * Description: It tells if lower case
+ * @grid: - An input parameter
+ * @height: -  by a new line
+ * Return: 1 when true and 0 when false
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+	return;
+}
