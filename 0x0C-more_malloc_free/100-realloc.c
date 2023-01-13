fef67009eb6b06bcf1891a4e0d69b680ecca85e2
Chekwasy <richardchekwas@gmail.com>
2023-01-12 17:26:11 -0800

    q100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,24 +4,25 @@
 #include <string.h>
 
 /**
- * simple_print_buffer - prints buffer in hex
- * @buffer: the address of memory to print
- * @size: the size of the
+ * _realloc - prints buffer in hex
+ * @ptr: the address of memory to print
+ * @old_size: the size of the
+ * @new_size: - new size
  * Return: Nothing.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
- {
-	 void *pt;
+{
+	void *pt;
 
-	 if (old_size == new_size)
-		 return (ptr);
-	 if (new_size == 0 && ptr != NULL)
-	 {
-		 free(ptr);
-		 return(NULL);
-	 }
-	 pt = realloc(ptr, new_size);
-	 if (pt == NULL)
-		 return (NULL);
-	 return (pt);
- }
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	pt = realloc(ptr, new_size);
+	if (pt == NULL)
+		return (NULL);
+	return (pt);
+}
