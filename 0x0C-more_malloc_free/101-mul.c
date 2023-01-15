ab22b8ffad7bb6e67b5bb2858c40a527e36b8bb9
Chekwasy <richardchekwas@gmail.com>
2023-01-15 12:14:55 -0800

    q101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,9 +13,13 @@ void main(int argc, char *argv[])
 {
 	char *num1;
 	char *num2;
-	int len1;
-	int len2;
-	int a;
+	int **res;
+	int *nm1;
+	int *nm2;
+	int len1, l = 1;
+	int len2, uslen, len3;
+	int a, b, c;
+	int rem = 0;
 	int chk;
 
 	if (argc != 3)
@@ -25,9 +29,23 @@ void main(int argc, char *argv[])
 		return;
 	}
 	len1 = strlen(argv[1]);
-	len2 = strlen(arg[2]);
-	num1 = argv[1];
-	num2 = argv[2];
+	len2 = strlen(argv[2]);
+	if (len1 >= len2)
+	{
+		num1 = argv[1];
+		num2 = argv[2];
+	}
+	else
+	{
+		len1 = strlen(argv[2]);
+		len2 = strlen(argv[1]);
+		num1 = argv[2];
+		num2 = argv[1];
+	}
+	nm1 = malloc(sizeof(int) * len1);
+	nm2 = malloc(sizeof(int) * len2);
+	if (num1 == NULL || num2 == NULL)
+		return;
 	for (a = 0; a < len1; a++)
 	{
 		chk = *(num1 + a) - 48;
@@ -37,6 +55,7 @@ void main(int argc, char *argv[])
 			exit(98);
 			return;
 		}
+		nm1[a] = chk;
 	}
 	for (a = 0; a < len2; a++)
 	{
@@ -47,7 +66,85 @@ void main(int argc, char *argv[])
 			exit(98);
 			return;
 		}
+		nm2[a] = chk;
 	}
-	if (len1 >= len2)
-	printf("%c\n", *(s + 2));
+	res = calloc(len3, sizeof(int *));
+	if (res == NULL)
+		return;
+	len3 = len1 + len2;
+	for (a = 0; a < len3; a++)
+	{
+		res[a] = calloc((len2 + 1), sizeof(int));
+		if (res[a] == NULL)
+		{
+			for (; a >= 0; a--)
+			{
+				free(res[a]);
+				free(res);
+				return;
+			}
+		}
+	}
+	c = 0;
+	uslen = len3 - 1;
+	b = (len2 - 1);
+	for (; b >= 0; b--)
+	{
+		l++;
+		a = len1 - 1;
+		for (; a >= 0; a--)
+		{
+			//printf("b = %d a = %d\n", b, a);
+			chk = (nm2[b] * nm1[a]) + rem;
+			rem = 0;
+			//printf("chk = %d\n", chk);
+			if (chk / 10 != 0)
+				rem = rem + (chk / 10);
+			else
+				rem = 0;
+			//printf("rem = %d\n", rem);
+			res[uslen][c] = chk % 10;
+			//printf("%d", res[uslen][c]);
+			uslen--;
+		}
+		res[uslen][c] = rem;
+		//printf("%d", res[uslen][c]);
+		rem = 0;
+		//printf("\n");
+		c++;
+		uslen = len3 - l;
+	}
+	a = len3 - 1;
+	rem = 0;
+	chk = 0;
+	for (; a >= 0; a--)
+	{
+		for (b = 0; b < len2; b++)
+		{
+			chk += res[a][b];
+			//printf("%d", res[a][b]);
+		}
+		chk = chk + rem;
+		if (chk > 9)
+		{
+			res[a][len2] = chk % 10;
+			rem = chk / 10;
+		}
+		else
+		{
+			res[a][len2] = chk;
+			rem = 0;
+		}
+		//printf("%d", res[a][len2]);
+		chk = 0;
+	}
+	//printf("\n");
+	for (a = 0; a < len3; a++)
+	{
+		if (res[a][len2] != 0 && a == 0)
+			printf("%d", res[a][len2]);
+		if (a != 0)
+			printf("%d", res[a][len2]);
+	}
+	printf("\n");
 }
