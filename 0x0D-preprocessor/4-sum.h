93589a359201cc745632a8e6b24544c537735396
Chekwasy <richardchekwas@gmail.com>
2023-01-17 06:02:04 -0800

    q4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Richard Chukwuchekwa
+ * Desc: Header file that defines a function SUM(x,y)
+ *       that computes sum x and y
+ */
+
+#define SUM(x,y) (x) + (y)
+
+#endif
