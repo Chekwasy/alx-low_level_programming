ee743a8170e03882263aa13f4db2f403b8432f67
Chekwasy <richardchekwas@gmail.com>
2023-01-17 07:46:36 -0800

    q0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - structures
+ * @name: - name of user
+ * @age: - age of user
+ * @owner: - owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
