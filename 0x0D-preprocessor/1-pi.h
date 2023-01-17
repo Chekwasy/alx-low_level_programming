8903649955d83d7cfca0411d0411b9e2c27af9a0
Chekwasy <richardchekwas@gmail.com>
2023-01-17 04:08:03 -0800

    q1

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef 1-pi.h
+#define 1-pi.h
+
+/*
+ * File: 1-pi.h
+ * Auth: Richard Chukwuchekwa
+ * Desc: Header file that defines a macro named
+ *       PI as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
