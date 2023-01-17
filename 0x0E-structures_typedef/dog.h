e4a13b7e47367a7da2d07545146e7d41de586f56
Chekwasy <richardchekwas@gmail.com>
2023-01-17 12:21:52 -0800

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
