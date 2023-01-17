6cf7565f0752cb1e51d23399a8f7592e1ae21b8c
Chekwasy <richardchekwas@gmail.com>
2023-01-17 05:30:15 -0800

    q3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,8 +4,8 @@
 /*
  * File: 3-function_like_macro.h
  * Auth: Richard Chukwuchekwa
- * Desc: Header file that defines a macro named
- *       ABS(x) for absolute value.
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
