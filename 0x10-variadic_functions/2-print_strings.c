f29e532a9da791145015540acea7cd79cf5b9223
Chekwasy <richardchekwas@gmail.com>
2023-01-19 05:53:46 -0800

    q2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_strings - prints buffer in h
+ * @separator: the address of memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(ap, char*) == NULL)
+			printf("(nil)");
+		if (va_arg(ap, char*) != NULL)
+			printf("%s", va_arg(ap, char*));
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
