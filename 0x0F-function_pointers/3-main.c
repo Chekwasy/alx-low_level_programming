c8a7cc42f68c666bf53739b91937ad7aff9d6d13
Chekwasy <richardchekwas@gmail.com>
2023-01-21 05:24:41 -0800

    q3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,10 +9,10 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *operator;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -21,20 +21,23 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	operator = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if ((num2 == 0 && *operator == '%') ||
-	    (num2 == 0 && *operator == '/'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	if (get_op_func(operator) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
