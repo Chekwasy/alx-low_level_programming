d85b9c299d5e1ed12ea568c03f77854c73f66d89
Chekwasy <richardchekwas@gmail.com>
2023-01-21 04:56:16 -0800

    q3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -38,9 +38,6 @@ int main(int argc, char *argv[])
 		exit(99);
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", get_op_func(operator)(num1, num2));
-	}
+	printf("%d\n", get_op_func(operator)(num1, num2));
 	return (0);
 }
