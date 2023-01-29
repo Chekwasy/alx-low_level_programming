06a0cccd02d1d683b634b346364df5df1e906992
Chekwasy <richardchekwas@gmail.com>
2023-01-28 23:32:12 -0800

    q100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_b4(void) __attribute__ ((constructor));
+void print_b4(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
