b4c5deb7cd698053e6492dce05e06da8e242b37c
Chekwasy <richardchekwas@gmail.com>
2023-02-08 04:49:59 -0800

    q1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fileRep < 0)
 		return (-1);
 	len = strlen(text_content);
-	wrt = write(fileRep, text_content, len);
+	wrt = write(fileRep, text_content, len + 1);
 	if (wrt < 0)
 		return (-1);
 	close(fileRep);
