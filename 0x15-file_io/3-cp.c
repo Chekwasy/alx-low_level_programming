c2cb74258a10143de599bbd1f59425a8701fb2b5
Chekwasy <richardchekwas@gmail.com>
2023-02-09 08:04:42 -0800

    q3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,86 +4,72 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-int safe_close(int);
+#include <stdlib.h>                                        int close_file(int fl);
 /**
- * main - Main function to copy files
- * @argc: The number of passed arguments
+ * main - Main function to copy files                       * @argc: The number of passed arguments
  * @argv: The pointers to array arguments
  * Return: 1 on success, exits on failure
  */
 int main(int argc, char *argv[])
 {
-	char buffer[1024];
-	int bytes_read = 0, _EOF = 1, from_fd = -1, to_fd = -1, error = 0;
+	int file1, file2, rd = 1, wrt;
+	char ptr[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp %s %s\n", argv[1], a\
+			rgv[1]);
 		exit(97);
 	}
-
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd < 0)
+	file1 = open(argv[1], O_RDONLY | O_CREAT);
+	if (file1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		close_file(file1);
 		exit(98);
 	}
-
-	to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (to_fd < 0)
+	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		safe_close(from_fd);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close_file(file1);
+		close_file(file2);
 		exit(99);
 	}
-
-	while (_EOF)
+	while (rd > 0)
 	{
-		_EOF = read(from_fd, buffer, 1024);
-		if (_EOF < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			safe_close(from_fd);
-			safe_close(to_fd);
-			exit(98);
-		}
-		else if (_EOF == 0)
+		rd = read(file1, ptr, 1024);
+		if (rd < 0)
+			return (0);
+		if (rd == 0)
 			break;
-		bytes_read += _EOF;
-		error = write(to_fd, buffer, _EOF);
-		if (error < 0)
+		wrt = write(file2, ptr, rd);
+		if (wrt < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			safe_close(from_fd);
-			safe_close(to_fd);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close_file(file1);
+			close_file(file2);
 			exit(99);
 		}
 	}
-	error = safe_close(to_fd);
-	if (error < 0)
+	close_file(file1);
+	close_file(file2);
+	return (1);
+}
+/**
+ * close_file - Close files
+ * @fl: file
+ * Return: 1 on success
+ */
+int close_file(int fl)
+{
+	int cl;
+
+	cl = close(fl);
+	if (cl < 0)
 	{
-		safe_close(from_fd);
+		dprintf(2, "Error: Can't close fd %i\n", cl);
 		exit(100);
 	}
-	error = safe_close(from_fd);
-	if (error < 0)
-		exit(100);
-	return (0);
-}
-
-/**
- * safe_close - A function that closes a file and prints error when closed file
- * @description: Description error for closed file
- * Return: 1 on success, -1 on failure
- */
-int safe_close(int description)
-{
-	int error;
-
-	error = close(description);
-	if (error < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", description);
-	return (error);
+	return (1);
 }
