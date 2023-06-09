c3d54808f3c72599cab9e3ebd82e211ecdd25973
root <richardchekwas@gmail.com>
2023-06-08 23:18:54 -0700

    qq

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = NULL;
 	unsigned long int i = 0;
 
-	if (size <= 0)
+	if (size <= 0 || size == NULL)
 		return (NULL);
 	ht = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (ht == NULL)
