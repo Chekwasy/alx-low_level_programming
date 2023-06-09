72bf7df6bc9522ba35aafb51850cb682922c809a
root <richardchekwas@gmail.com>
2023-06-08 21:15:56 -0700

    qqy

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - setting hash table
+ * @ht:- created hash table
+ * @key:- key string to use
+ * @value:- value of the string
+ * Return: 1 or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int hs;
+	hash_node_t *hn = NULL;
+
+	if (strlen(key) == 0 || key == NULL)
+		return (0);
+	if (value == NULL)
+		return (0);
+	hs = key_index((unsigned char *)key, ht->size);
+	hn = malloc(sizeof(hash_node_t *));
+	if (hn == NULL)
+		return (0);
+	hn->key = malloc(sizeof(char *));
+	if (hn->key == NULL)
+		return (0);
+	hn->value = malloc(sizeof(char *));
+	if (hn->value == NULL)
+		return (0);
+	hn->next = NULL;
+	hn->key = (char *)key;
+	hn->value = (char *)value;
+	if (ht->array[hs] == NULL)
+	{
+		ht->array[hs] = hn;
+		return (1);
+	}
+	else
+	{
+		ht->array[hs]->next = hn;
+		return (1);
+	}
+}
