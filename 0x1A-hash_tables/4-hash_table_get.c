0095810df2745d6830babb277b0af2840969e39b
root <richardchekwas@gmail.com>
2023-06-09 12:12:53 -0700

    qqy

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,17 +17,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	idx = key_index((unsigned char *)key, ht->size);
 	if (ht->array[idx] != NULL && strcmp(ht->array[idx]->key, key) == 0)
 		return (ht->array[idx]->value);
-	else if (ht->array[idx] == NULL)
+	if (ht->array[idx] == NULL)
 		return (NULL);
-	else
+	temp = ht->array[idx]->next;
+	while (temp != NULL && temp)
 	{
-		temp = ht->array[idx]->next;
-		while (temp != NULL && temp)
-		{
-			if (strcmp(temp->key, key) == 0)
-				return (temp->value);
-			temp = temp->next;
-		}
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
 	}
 	return (NULL);
 }
