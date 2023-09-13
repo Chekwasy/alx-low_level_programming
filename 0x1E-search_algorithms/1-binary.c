4cc6ba02bb92990ea6414e57b1f2b301ee9bbd3e
Richard <richardchekwas@gmail.com>
2023-09-13 11:14:35 +0100

    aa

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * binary_search - Entry point
+ * @array: - array to work on
+ * @size: - size of array
+ * @value: - value to search for
+ * Return: value found or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t L = 0, R = size - 1;
+	int to = size - 1;
+	int i = 0, m = 0;
+
+	while (L <= R)
+	{
+		printf("Searching in array: ");
+		while (i <= to)
+		{
+			printf("%d", array[i]);
+			if (i != to)
+				printf(", ");
+			else
+				printf("\n");
+			i++;
+		}
+		m = floor((L + R) / 2);
+		if (array[m] < value)
+		{
+			L = m + 1;
+			to = R;
+			i = L;
+		}
+		else if (array[m] > value)
+		{
+			R = m - 1;
+			to = R;
+			i = L;
+		}
+		else
+			return (m);
+	}
+	return (-1);
+}
